8ab25e491c7ad5204b2815e7004fb6f409826bce
Dee-Jay Tagubasi <dee-jay.p.tagubasi@accenture.com>
2018-08-06 23:28:45 +0800

    added Basemodel as reusable class for DB data processing

---
+++ services/sqlalchemy.py
@@ -0,0 +1,35 @@
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+import logging
+
+db = SQLAlchemy()
+
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    meta_created_on = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+    meta_updated_on = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+
+    @classmethod
+    def all(cls):
+        return db.session.query(cls).all()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def update(self):
+        db.session.commit()
+        return self
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def refresh(self):
+        db.session.refresh(self)
+
+    def to_dict(self):
+        return dict((k, getattr(self, k)) for k in self.__table__.c.keys())
