3e9a7a0ca9cf7985d471a5d1a3f4ade84d0ef0d5
Dee-Jay Tagubasi <dee-jay.p.tagubasi@accenture.com>
2018-08-11 17:49:25 +0800

    updated the relationship to many-to-many

--- project/models/Item.py
+++ project/models/Item.py
@@ -1,5 +1,4 @@
 from project.services.sqlalchemy import BaseModel, db
-from sqlalchemy import ForeignKey
 
 
 class Item(BaseModel):
@@ -8,4 +7,3 @@ class Item(BaseModel):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     quantity = db.Column(db.Integer)
-    shopping_list_id = db.Column(db.Integer, ForeignKey('shopping_list.id'))
--- project/models/ShoppingList.py
+++ project/models/ShoppingList.py
@@ -1,6 +1,12 @@
 from project.services.sqlalchemy import BaseModel, db
+from sqlalchemy import ForeignKey
 from project.models.Item import Item
 import datetime
+
+association_table = db.Table('association', db.Model.metadata,
+                             db.Column('shopping_list_id', db.Integer, ForeignKey('shopping_list.id')),
+                             db.Column('item_id', db.Integer, ForeignKey('item.id'))
+)
 
 
 class ShoppingList(BaseModel):
@@ -10,12 +16,13 @@ class ShoppingList(BaseModel):
     title = db.Column(db.String(100), index=True, nullable=False)
     store_name = db.Column(db.String(200), nullable=False)
     created_on = db.Column(db.DateTime, default=datetime.datetime.utcnow())
-    items = db.relationship('Item', backref=db.backref('shopping_list', lazy=True))
+    items = db.relationship('Item', secondary=association_table, backref=db.backref('shopping_list', lazy=True))
 
     @classmethod
     def get_by_item_id(cls, item_id):
-        return db.session.query(cls).join(Item, cls.items).filter(Item.id == item_id)
+        return db.session.query(cls).join(Item, cls.items).filter(Item.item_ref_id == item_id)
 
     @classmethod
     def get_by_item_name_wildcard(cls, item_name):
-        return db.session.query(cls).join(Item, cls.items).filter(Item.name.like('%' + item_name + '%')).all()
+        return db.session.query(cls).join(Item, cls.items)\
+                 .filter(Item.item_id.name.like('%' + item_name + '%')).all()
